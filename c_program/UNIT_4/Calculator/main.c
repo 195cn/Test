0655be956d70de8b2d530d852308e65c8f036c66
195cn <2259761058@qq.com>
2018-09-29 20:30:45 +0800

    把计算器分割成小文件

--- c_program/UNIT_4/Calculator/main.c
+++ c_program/UNIT_4/Calculator/main.c
@@ -2,13 +2,9 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include"calc.h"
 
 #define MAXOP	100
-#define NUMBER	'0'
-
-int getop(char []);
-void push(double);
-double pop(void);
 
 int main()
 {
@@ -49,74 +45,4 @@ int main()
 		}
 	}
 	return 0;
-}
-
-#define MAXVAL 100
-
-int sp = 0;
-double val[MAXVAL];
-
-void push(double f)
-{
-	if(sp < MAXVAL)
-		val[sp++] = f;
-	else
-		printf("error: stack full,can't push %g\n",f);
-}
-
-double pop(void)
-{
-	if(sp > 0)
-		return val[--sp];
-	else
-	{
-		printf("error: stack empty\n");
-		return 0.0;
-	}
-}
-
-#include<ctype.h>
-
-int getch(void);
-void ungetch(int);
-
-//取下一个操作符
-int getop(char s[])
-{
-	int i,c;
-
-	while((s[0] = c = getch()) == ' '|| c == '\t')
-		;
-	s[1] = '\0';
-	if(!isdigit(c) && c != '.')
-		return c;
-	i = 0;
-	if(isdigit(c))
-		while(isdigit(s[++i] = c = getch()))
-			;
-	if(c == '.')
-		while(isdigit(s[++i] = c = getch()))
-			;
-	s[i] = '\0';
-	if(c != EOF)
-		ungetch(c);
-	return NUMBER;
-}
-
-#define BUFSIZE 100
-
-char buf[BUFSIZE];
-int bufp = 0;
-
-int getch(void)
-{
-	return (bufp > 0) ? buf[--bufp] : getchar();
-}
-
-void ungetch(int c)
-{
-	if(bufp >= BUFSIZE)
-		printf("ungetch: too many characters\n");
-	else 
-		buf[bufp++] = c;
 }
