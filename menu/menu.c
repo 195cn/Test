907d76e173606e8aa55d25581a69a4f527a0d9c0
shiyanlou <2259761058@qq.com>
2018-10-26 23:41:28 +0800

    write menu.c with datanode

--- menu/menu.c
+++ menu/menu.c
@@ -13,12 +13,37 @@ History		:
 
 #define TRUE 1
 #define FALSE 0
+#define CMD_MAX_LEN 128
+int Help();
+int Quit();
 
-void help()
+typedef struct DataNode
 {
-	printf("Show help\n");
+	char *cmd;
+	char *desc;
+	int (*handler)();
+	struct DataNode *next;
+}tDataNode;
+
+static tDataNode head[]=
+{
+	{"help","This is help command.",Help,&head[1]},
+	{"version","Menu version v1.0",NULL,&head[2]},
+	{"quit","Quit from menu",Quit,NULL}
+};
+
+int Help()
+{
+	printf("Menu list:\n");
+	tDataNode *p = head;
+	while(p != NULL)
+	{
+		printf("%s - %s\n",p->cmd,p->desc);
+		p = p->next;
+	}
+	return 0;
 }
-void quit()
+int Quit()
 {
 	exit(0);
 }
@@ -29,20 +54,24 @@ void others()
 
 int main()
 {
-	char cmd[128];
+	//cmd line begins
 	while(TRUE)
 	{
+		char cmd[CMD_MAX_LEN];
 		printf("menu>");
 		scanf("%s",cmd);
-		if(!strcmp(cmd,"help"))
-		{
-			help();
-		}else if(!strcmp(cmd,"quit"))
-		{
-			quit();
-		}else
-		{
-			others();
+		tDataNode *p = head;
+		while(p != NULL) {
+			if(strcmp(p->cmd,cmd) == 0)
+			{
+				printf("%s - %s\n",p->cmd,p->desc);
+				if(p->handler != NULL)
+				{
+					p->handler();
+				}
+				break;
+			}
+			p = p->next;
 		}
 	}
 }
