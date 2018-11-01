5dfe2c50bfd3466744b3ec4f0e606b6dd50445f8
shiyanlou <2259761058@qq.com>
2018-11-01 22:33:45 +0800

    perk.h

--- Fallout_76_Perks/perk.h
+++ Fallout_76_Perks/perk.h
@@ -3,7 +3,7 @@
  *Author        :195cn
  *Descrpiton    :Fallout 76 perk.h
  *Date          :2018/11/1
- *Version       :0.01
+ *Last date     :2018/11/1
  ******************************************/
 #define S 0
 #define P 1
@@ -31,6 +31,8 @@ typedef struct perk
 
 tPerk perk_list[]=
 {
-    {0,S,"Test","This is test perk",10,3,NULL}
+    {0,S,"Test","This is test perk",10,3,&perk_list[1]},
+    {1,S,"Bandolier","Ammo weight 90%% less",0,3,NULL}
 };
 
+int showAll();
